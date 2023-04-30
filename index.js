677110cca4f3b33e1a1e4ec18a7caea5e0bf7810
Emily Yao <emilyyao9898_@hotmail.com>
2023-04-30 01:08:43 -0700

    add user form & submit user to array

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+//Users and Passwords (in memory 'database')
+var users = []; 
+
+
 app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req,res) => {
@@ -31,6 +35,7 @@ app.get('/contact', (req,res) => {
     res.send(html);
 });
 
+
 app.post('/submitEmail', (req,res) => {
     var email = req.body.email;
     if (!email) {
@@ -40,6 +45,35 @@ app.post('/submitEmail', (req,res) => {
         res.send("Thanks for subscribing with your email: "+email);
     }
 });
+
+app.get('/createUser', (req,res) => {
+    var html = `
+    <form action='/submitUser' method='post'>
+    <input name='username' type='text' placeholder='username'>
+    <input name='password' type='password' placeholder='password'>
+    <button>Submit</button>
+    </form>
+    `;
+    res.send(html);
+});
+
+app.post('/submitUser', (req,res) => {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    users.push({ username: username, password: password });
+
+    console.log(users);
+
+    var usershtml = "";
+    for (i = 0; i < users.length; i++) {
+        usershtml += "<li>" + users[i].username + ": " + users[i].password + "</li>";
+    }
+
+    var html = "<ul>" + usershtml + "</ul>";
+    res.send(html);
+});
+
 
 
 app.get('/cat/:id', (req,res) => {
@@ -60,10 +94,12 @@ app.get('/cat/:id', (req,res) => {
 
 app.use(express.static(__dirname + "/public"));
 
+
 app.get("*", (req,res) => {
 	res.status(404);
 	res.send("Page not found - 404");
 })
+
 
 app.listen(port, () => {
 	console.log("Node application listening on port "+port);
