afec5e0bc7fe980e89a2e41a265d47cade7d1efd
Emily Yao <emilyyao9898_@hotmail.com>
2023-04-30 00:41:40 -0700

    add emial form, cat pictures

--- index.js
+++ index.js
@@ -4,9 +4,66 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(express.urlencoded({extended: false}));
+
 app.get('/', (req,res) => {
     res.send("<h1>Hello World!</h1>");
 });
+
+app.get('/about', (req,res) => {
+    var color = req.query.color;
+
+    res.send("<h1 style='color:"+color+";'>Emily Yao</h1>");
+});
+
+app.get('/contact', (req,res) => {
+    var missingEmail = req.query.missing;
+    var html = `
+        email address:
+        <form action='/submitEmail' method='post'>
+            <input name='email' type='text' placeholder='email'>
+            <button>Submit</button>
+        </form>
+    `;
+    if (missingEmail) {
+        html += "<br> email is required";
+    }
+    res.send(html);
+});
+
+app.post('/submitEmail', (req,res) => {
+    var email = req.body.email;
+    if (!email) {
+        res.redirect('/contact?missing=1');
+    }
+    else {
+        res.send("Thanks for subscribing with your email: "+email);
+    }
+});
+
+
+app.get('/cat/:id', (req,res) => {
+
+    var cat = req.params.id;
+
+    if (cat == 1) {
+        res.send("Kiwi: <img src='/cat1.jpeg' style='width:250px;'>");
+    }
+    else if (cat == 2) {
+        res.send("Noya: <img src='/cat2.avif' style='width:250px;'>");
+    }
+    else {
+        res.send("Invalid cat id: "+cat);
+    }
+});
+
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("*", (req,res) => {
+	res.status(404);
+	res.send("Page not found - 404");
+})
 
 app.listen(port, () => {
 	console.log("Node application listening on port "+port);
